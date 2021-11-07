05fd2b1efed256f400943e1f2194510857a483ef
Tomeski05 <tomeski@protonmail.com>
2021-11-07 19:33:14 +0100

    Lamazon

---
+++ Lamazon/Lamazon/DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using Lamazon.DataAccess.Interfaces;
+using Lamazon.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lamazon.DataAccess.Repositories
+{
+    public class OrderRepository : BaseRepository, IRepository<Order>
+    {
+        public OrderRepository(LamazonDbContext context) : base(context) { }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lamazon/Lamazon/Services/Helpers/DIModule.cs
@@ -0,0 +1,32 @@
+﻿using Lamazon.DataAccess;
+using Lamazon.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lamazon.Services.Helpers
+{
+    public static class DIModule
+    {
+        public static IServiceCollection RegisterModule(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<LamazonDbContext>
+                (options => options.UseSqlServer(connectionString));
+
+            services
+            .AddIdentity<User, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoleManager<RoleManager<IdentityRole>>()
+            .AddEntityFrameworkStores<LamazonDbContext>()
+            .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
